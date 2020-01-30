246fe7d8ecc6ec741015bd4eca8cb2d52605562a
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-01-30 21:49:24 +0000

    Add logic to all buttons

--- app/src/main/java/com/example/androidcalculator/MainActivity.java
+++ app/src/main/java/com/example/androidcalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,24 +23,175 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button0 = (Button) findViewById(R.id.button0);
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
-        button4 = (Button) findViewById(R.id.button4);
-        button5 = (Button) findViewById(R.id.button5);
-        button6 = (Button) findViewById(R.id.button6);
-        button7 = (Button) findViewById(R.id.button7);
-        button8 = (Button) findViewById(R.id.button8);
-        button9 = (Button) findViewById(R.id.button9);
-        button10 = (Button) findViewById(R.id.button10);
-        buttonAdd = (Button) findViewById(R.id.buttonAdd);
-        buttonSub = (Button) findViewById(R.id.buttonSub);
-        buttonMul = (Button) findViewById(R.id.buttonMul);
-        buttonDiv = (Button) findViewById(R.id.buttonDiv);
-        buttonEqual = (Button) findViewById(R.id.buttonEqual);
-        buttonC = (Button) findViewById(R.id.buttonC);
-        calculatorTextField = (EditText) findViewById(R.id.calculatorTextField);
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        button10 = findViewById(R.id.button10);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonSub = findViewById(R.id.buttonSub);
+        buttonMul = findViewById(R.id.buttonMul);
+        buttonDiv = findViewById(R.id.buttonDiv);
+        buttonEqual = findViewById(R.id.buttonEqual);
+        buttonC = findViewById(R.id.buttonC);
+        calculatorTextField = findViewById(R.id.calculatorTextField);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText(calculatorTextField.getText() + "1");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText(calculatorTextField.getText() + "2");
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText(calculatorTextField.getText() + "3");
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText(calculatorTextField.getText() + "4");
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText(calculatorTextField.getText() + "5");
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText(calculatorTextField.getText() + "6");
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText(calculatorTextField.getText() + "7");
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText(calculatorTextField.getText() + "8");
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText(calculatorTextField.getText() + "9");
+            }
+        });
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText(calculatorTextField.getText() + "0");
+            }
+        });
+
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText(calculatorTextField.getText() + ".");
+            }
+        });
+
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatorTextField.setText("");
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (calculatorTextField == null) {
+                    calculatorTextField.setText("");
+                } else {
+                    numValueOne = Float.parseFloat(calculatorTextField.getText() + "");
+                    calcAdd = true;
+                    calculatorTextField.setText("");
+                }
+
+            }
+        });
+
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numValueOne = Float.parseFloat(calculatorTextField.getText() + "");
+                calcSub = true;
+                calculatorTextField.setText("");
+            }
+        });
+
+        buttonMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numValueOne = Float.parseFloat(calculatorTextField.getText() + "");
+                calcMul = true;
+                calculatorTextField.setText("");
+            }
+        });
+
+        buttonDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numValueOne = Float.parseFloat(calculatorTextField.getText() + "");
+                calcDiv = true;
+                calculatorTextField.setText("");
+            }
+        });
+
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numValueTwo = Float.parseFloat(calculatorTextField.getText() + "");
+
+                if (calcAdd == true) {
+                    calculatorTextField.setText(numValueOne + numValueTwo + "");
+                    calcAdd = false;
+                }
+                if (calcSub == true) {
+                    calculatorTextField.setText(numValueOne - numValueTwo + "");
+                    calcSub = false;
+                }
+                if (calcMul == true) {
+                    calculatorTextField.setText(numValueOne * numValueTwo + "");
+                    calcMul = false;
+                }
+                if (calcDiv == true) {
+                    calculatorTextField.setText(numValueOne / numValueTwo + "");
+                    calcDiv = false;
+                }
+            }
+        });
+
+
 
 
     }
