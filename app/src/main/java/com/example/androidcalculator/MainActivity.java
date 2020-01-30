12df85e4ab71d2c1d72c49bc286ece6e7ec8c9d3
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-01-30 20:11:43 +0000

    Declare button variables

--- app/src/main/java/com/example/androidcalculator/MainActivity.java
+++ app/src/main/java/com/example/androidcalculator/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.androidcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button0, button1, button2, button3, button4, button5, button6, button7, button8,
+            button9, buttonAdd, buttonSub, buttonDiv, buttonMul, button10, buttonC, buttonEqual;
+
+    EditText calculatorTextField;
+
+    float numValueOne, numValueTwo;
+
+    boolean calcAdd, calcSub, calcMul, calcDiv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        button10 = (Button) findViewById(R.id.button10);
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonSub = (Button) findViewById(R.id.buttonSub);
+        buttonMul = (Button) findViewById(R.id.buttonMul);
+        buttonDiv = (Button) findViewById(R.id.buttonDiv);
+        buttonEqual = (Button) findViewById(R.id.buttonEqual);
+        buttonC = (Button) findViewById(R.id.buttonC);
+        calculatorTextField = (EditText) findViewById(R.id.calculatorTextField);
+
+
     }
 }
